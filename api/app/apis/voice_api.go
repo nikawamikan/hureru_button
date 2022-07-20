f7adba73ed053f8a49b276e276337c692521f21f
nikawamikan <wkwktktkkrkr@outlook.jp>
2022-07-21 01:54:21 +0900

    APIの構築だいたいOK

---
+++ api/app/apis/voice_api.go
@@ -0,0 +1,88 @@
+/*
+フロントエンドで利用するJSONとMP3データを提供するAPI
+*/
+package apis
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"hureru_button.com/hureru_button/db"
+)
+
+var PREFIX string
+
+func SetInit(prefix string) {
+	PREFIX = prefix + "hurerubutton/api/voice/"
+}
+
+type VoiceJson struct {
+	Name    string `json:"name"`
+	Read    string `json:"read"`
+	Address string `json:"address"`
+	AttrIds []uint `json:"attrIds"`
+}
+
+type VoiceListJson struct {
+	Prefix    string          `json:"prefix"`
+	AttrTypes map[uint]string `json:"attrType"`
+	Voices    []VoiceJson     `json:"voices"`
+}
+
+func SetApis(e echo.Echo) {
+	e.GET("/voicelist", voicelist)
+	e.GET("/voice/:name", getVoice)
+}
+
+func voicelist(c echo.Context) error {
+	return c.JSON(http.StatusOK, voiceListBuilder())
+}
+
+// Voiceデータを送信する
+func getVoice(c echo.Context) error {
+	name := c.Param("name")
+	return c.File(fmt.Sprintf("./voice/%s", name))
+}
+
+func voiceListBuilder() VoiceListJson {
+	getAttrsMap := func() map[uint]string {
+		attrTypes := db.GetAttrTypes()
+		attrsMap := make(map[uint]string)
+		for _, obj := range attrTypes {
+			attrsMap[obj.ID] = obj.Name
+		}
+		return attrsMap
+	}
+	getVoiceList := func() []VoiceJson {
+		voices := db.GetVoices()
+		voiceList := []VoiceJson{}
+		getAttrIdMap := func() map[uint64][]uint {
+			attrs := db.GetAttrs()
+			attrMap := make(map[uint64][]uint)
+			for _, obj := range attrs {
+				attrMap[obj.VoiceID] = append(attrMap[obj.VoiceID], obj.TypeID)
+			}
+			return attrMap
+		}
+		attrMap := getAttrIdMap()
+		getAttrIds := func(i uint64) []uint {
+			if val, ok := attrMap[i]; ok {
+				return val
+			}
+			return []uint{}
+		}
+		for _, obj := range voices {
+			voiceList = append(voiceList,
+				VoiceJson{
+					Name:    obj.Name,
+					Read:    obj.Read,
+					Address: obj.Address,
+					AttrIds: getAttrIds(obj.ID),
+				},
+			)
+		}
+		return voiceList
+	}
+	return VoiceListJson{Prefix: PREFIX, AttrTypes: getAttrsMap(), Voices: getVoiceList()}
+}
--- api/app/main.go
+++ api/app/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "hureru_button.com/hureru_button/dblink"
+import (
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"hureru_button.com/hureru_button/apis"
+	"hureru_button.com/hureru_button/db"
+)
 
 func main() {
-	dblink.FindAttr()
+	// 定数のセット
+	db.SetInit(os.Getenv("MYSQL_ROOT_PASSWORD"), "root", "hureru_button_db", "hureru_voice")
+	apis.SetInit(os.Getenv("ADDRESS_BASE"))
+
+	// APIを設定
+	e := echo.New()
+	apis.SetApis(*e)
+
+	// APIを常駐させる
+	e.Logger.Fatal(e.Start(":80"))
 }
